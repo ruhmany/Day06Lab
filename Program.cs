d78d71fa4fe4d79860c3b080c69322d9c795fc59
Abdelrhman Ahmed <47417146+ZoRoOt3@users.noreply.github.com>
2023-09-25 13:32:36 +0300

    Hello World

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Products products = new Products() { ID =1, Price=19.5, DiscountPercentage=12.5, Name="Tomato"};
+            products.DiscountPercentage = 100;
+            products.DiscountChange += Products_DiscountChange;
+            products.DiscountPercentage = 100;
+            products.DiscountPercentage = 99;
+        }
+
+        private static void Products_DiscountChange(Products product, double discount)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
